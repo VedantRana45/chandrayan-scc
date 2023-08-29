0cde36b623987e702dca50d3870fa972789393b6
VR-007 <96051866+VR-007@users.noreply.github.com>
2023-08-29 23:35:59 +0530

    multiple commands handled

--- Chandrayan3.js
+++ Chandrayan3.js
@@ -10,6 +10,7 @@ class GalacticSpaceCraft {
         this.prevDirection = null;
     }
 
+    // function for moving space craft 1 step forward without changing direction
     moveForward() {
         switch (this.currDirection) {
             case 'N':
@@ -33,6 +34,7 @@ class GalacticSpaceCraft {
         }
     }
 
+    // function for moving space craft 1 step backward without changing direction
     moveBackward() {
         switch (this.currDirection) {
             case 'N':
@@ -56,6 +58,7 @@ class GalacticSpaceCraft {
         }
     }
 
+    // function for changing only the direction to left using prevDirection 
     moveLeft() {
         switch (this.currDirection) {
             case 'N':
@@ -121,6 +124,7 @@ class GalacticSpaceCraft {
         }
     }
 
+    // function for changing only the direction to right using prevDirection 
     moveRight() {
         switch (this.currDirection) {
             case 'N':
@@ -186,18 +190,50 @@ class GalacticSpaceCraft {
         }
     }
 
+    // function for changing only the direction to upward
     moveUpward() {
         this.prevDirection = this.currDirection;
         this.currDirection = 'U';
     }
 
+    // function for changing only the direction to downward
     moveDownward() {
         this.prevDirection = this.currDirection;
         this.currDirection = 'D';
     }
 
+    //function for getting coordinates and direction for single command
     getPosition() {
         return `Position : [ ${this.coordinates.x}, ${this.coordinates.y}, ${this.coordinates.z} ] Direction : ${this.currDirection}`;
+    }
+
+    //function for getting coordinates and direction for array of multiple commands
+    getCoordinates(directedSteps) {
+        for (const step of directedSteps) {
+            switch (step) {
+                case 'f':
+                    this.moveForward();
+                    break;
+                case 'b':
+                    this.moveBackward();
+                    break;
+                case 'l':
+                    this.moveLeft();
+                    break;
+                case 'r':
+                    this.moveRight();
+                    break;
+                case 'u':
+                    this.moveUpward();
+                    break;
+                case 'd':
+                    this.moveDownward();
+                    break;
+            }
+        }
+
+        var result = `Position : [ ${this.coordinates.x}, ${this.coordinates.y}, ${this.coordinates.z} ] Direction : ${this.currDirection}`;
+        return result;
     }
 }
 
